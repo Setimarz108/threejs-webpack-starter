59195d75814e644716ad11f32d47c82ab084a8b2
Setimarz108 <66173614+Setimarz108@users.noreply.github.com>
2022-03-07 16:24:24 +0100

    commit

--- src/script.js
+++ src/script.js
@@ -2,7 +2,11 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import gsap from 'gsap'
 
+//texture Loeader
+
+const textureLoader = new THREE.TextureLoader()
 // Debug
 const gui = new dat.GUI()
 
@@ -11,18 +15,34 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+const geometry = new THREE.PlaneBufferGeometry(1, 1.3)
+
+for (let i = 0; i < 4; i++) {
+
+  const material = new THREE.MeshBasicMaterial({
+      map: textureLoader.load(`/images/${i}.avif`)
+  })
+
+  const img= new THREE.Mesh(geometry, material)
+  img.position.set(Math.random()+.3, i * -1.8)
+ 
+  scene.add(img)
+
+}
+
+let objs = []
+
+scene.traverse((object)=>  {    
+    if( object.isMesh)
+    objs.push(object)
+
+    console.log(objs)
+})
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
-// Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
 
-// Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
 
 // Lights
 
@@ -55,6 +75,26 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+//mouse
+
+window.addEventListener("wheel", onMouseWheel)
+
+let y = 0
+let position = 0
+
+function onMouseWheel(event){
+
+    y = event.deltaY * 0.0007
+}
+
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (event) => {
+
+    mouse.x = event.clientX / sizes.width * 2 - 1
+    mouse.y = - (event.clientY / sizes.height) * 2 + 1
+})
+
 /**
  * Camera
  */
@@ -64,7 +104,7 @@ camera.position.x = 0
 camera.position.y = 0
 camera.position.z = 2
 scene.add(camera)
-
+gui.add(camera.position, 'y').min(-5).max(10)
 // Controls
 // const controls = new OrbitControls(camera, canvas)
 // controls.enableDamping = true
@@ -82,19 +122,39 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 
+const raycaster = new THREE.Raycaster()
+
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
 
     const elapsedTime = clock.getElapsedTime()
+      
+    position += y
+    y *= .9
 
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+ camera.position.y = - position
 
-    // Update Orbital Controls
-    // controls.update()
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObjects(objs)
 
+    for(const intersect of intersects) {
+              gsap.to(intersect.object.scale, {x:1.7, y:1.7})
+              gsap.to(intersect.object.rotation, {y: -.5})
+              gsap.to(intersect.object.position, {z: -0.9})
+
+    }
+
+    for (const object of objs){
+        if(!intersects.find(intersect => intersect.object === object)){
+          
+            gsap.to(object.scale, {x:1, y:1})
+            gsap.to(object.rotation, {y: 0})
+            gsap.to(object.position, {z: 0})
+        }
+    }
+     
     // Render
     renderer.render(scene, camera)
 
